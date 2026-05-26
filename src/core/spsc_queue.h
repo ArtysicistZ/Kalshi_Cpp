9b2a6261357aac75817f644f76a58f19f93607d1
Yincheng Zhou <artysicistz@outlook.com>
2026-05-26 15:36:13 -0400

    spsc

--- src/core/spsc_queue.h
+++ src/core/spsc_queue.h
@@ -1,7 +1,24 @@
 template<typename T, size_t Capacity>
 class SPSCQueue {
     T data_[Capacity];
-    alignas(64) std::atomic<size_t> head_;
-    alignas(64) std::atomic<size_t> tail_;
+    alignas(64) std::atomic<size_t> head_{0};
+                size_t cache_tail_{0};
+    alignas(64) std::atomic<size_t> tail_{0};
+                size_t cache_head_{0};
+
+    SPSCQueue() {
+        static_assert(
+            Capacity & (Capacity - 1) == 0,
+            "Capacity must be power of 2!"
+        );
+    }
+    ~SPSCQueue();
+
+    SPSCQueue(const SPSCQueue&) = delete;
+    SPSCQueue& operator=(const SPSCQueue&) = delete;
+
+    bool try_push(const T& item) {
+        
+    }
 
 };
